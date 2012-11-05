5e1c1021eeea5348b383c3e8d81daa1aef0b3d84
Mandimby RAVELOARINJAKA <mandimby.raveloarinjaka@gmail.com>
2012-11-05 15:25:03 -0500

    Introduce rotations and mirror to retrieve only free polyominos.

--- polyominos.erl
+++ polyominos.erl
@@ -1,5 +1,22 @@
 -module(polyominos).
--export([adjacents/1, translate/1, generate/1]).
+-export([adjacents/1, translate/1, generate/1, retrieveCanonicalForm/1]).
+
+rotate({X,Y}, Theta) -> 
+   {round(X*math:cos(Theta) - Y*math:sin(Theta)), round(X*math:sin(Theta) + Y*math:cos(Theta))}.
+
+rotate90(Polyomino) ->
+   translate(lists:sort(lists:map(fun (X) -> rotate(X, math:pi()/2) end, Polyomino))).
+rotate180(Polyomino) ->
+   translate(lists:sort(lists:map(fun (X) -> rotate(X, math:pi()) end, Polyomino))).
+rotate270(Polyomino) ->
+   translate(lists:sort(lists:map(fun (X) -> rotate(X, 3*math:pi()/2) end, Polyomino))).
+
+mirror(Polyomino) ->
+   translate(lists:sort(lists:map(fun ({X,Y}) -> {-X, Y} end, Polyomino))).
+
+retrieveCanonicalForm(Polyomino) ->
+   RetrieveAllRotations = fun (X) -> [X, rotate90(X), rotate180(X), rotate270(X)] end,
+   hd(lists:sort(RetrieveAllRotations(Polyomino)++RetrieveAllRotations(mirror(Polyomino)))).
 
 adjacentsToOneElement({X,Y}) -> [{X-1, Y}, {X, Y-1}, {X, Y+1}, {X+1, Y}].
 
@@ -8,7 +25,7 @@ adjacentsInternal([], _, AdjacentsSoFar) -> AdjacentsSoFar;
 adjacentsInternal([Current|Remainings], Polyomino, AdjacentsSoFar) ->
    PotentialAdjacents = adjacentsToOneElement(Current)--Polyomino,
    AdjacentsToAdd = PotentialAdjacents--AdjacentsSoFar,
-   adjacentsInternal(Remainings, Polyomino, AdjacentsSoFar++AdjacentsToAdd).
+   adjacentsInternal(Remainings, Polyomino, AdjacentsToAdd++AdjacentsSoFar).
 
 adjacents(Polyomino) -> adjacentsInternal(Polyomino, Polyomino, []).
 
@@ -17,7 +34,8 @@ translate([Head|Tail]) ->
    [{X-MinX, Y-MinY} || {X, Y} <- [Head|Tail]].
 
 generateFromOnePolyonimo(Polyomino) -> 
-   lists:map(fun (X) -> lists:sort(translate(X)) end, [Polyomino++[Adjacent] || Adjacent <- adjacents(Polyomino)]).
+   PotentialPolyominos = lists:map(fun (X) -> lists:sort(translate(X)) end, [[Adjacent]++Polyomino || Adjacent <- adjacents(Polyomino)]),
+   lists:usort([retrieveCanonicalForm(PotentialPolyomino) || PotentialPolyomino <- PotentialPolyominos]).
 
 generateInternal(0, GeneratedSoFar) -> GeneratedSoFar;
 generateInternal(N, GeneratedSoFar) -> 
